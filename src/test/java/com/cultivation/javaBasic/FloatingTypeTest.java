e4a57b35a0a1eb8f4e80e42dd9331c28b623c52d
Eddie Fong <fonged@oocl.com>
2018-11-12 11:05:21 +0800

    fix typo

--- src/test/java/com/cultivation/javaBasic/FloatingTypeTest.java
+++ src/test/java/com/cultivation/javaBasic/FloatingTypeTest.java
@@ -63,7 +63,7 @@ class FloatingTypeTest {
     @SuppressWarnings("unused")
     private boolean isNan(double realNumber) {
         // TODO: please find the method that performs this operation.
-        return Double.isNaN(realNumber)
+        return Double.isNaN(realNumber);
     }
 
     @SuppressWarnings("unused")
